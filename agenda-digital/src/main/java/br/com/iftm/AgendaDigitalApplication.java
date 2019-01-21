b692b8438d73f824f614588edf734e42bbec29db
Tiago Alcantara <tiagofacampos@gmail.com>
2019-01-21 14:46:35 -0200

    Carga do Projeto

---
+++ agenda-digital/src/main/java/br/com/iftm/AgendaDigitalApplication.java
@@ -0,0 +1,14 @@
+package br.com.iftm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDigitalApplication.class, args);
+	}
+
+}
+
